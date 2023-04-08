773da24014af560df8579dd02d1c5884273d2114
maximo tejeda <maximotejeda@gmail.com>
2023-04-08 16:59:19 -0400

    ADD user handlers

--- external/v0.1/router/user.go
+++ external/v0.1/router/user.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/maximotejeda/auth-service/external/v0.1/helper"
 )
@@ -10,7 +12,20 @@ func UserAddRoutes(r *gin.Engine) {
 	user := v01.Group("/user")
 	user.Use(helper.Validated())
 	{
-		user.GET("/info", nil)
-		user.POST("/interaction", nil)
+		user.GET("/info", userInfo)
+		user.GET("/interaction", userInteractions)
 	}
+}
+
+// userInfo returns information about the user
+func userInfo(c *gin.Context) {
+	username, _ := c.Get("username")
+	email, _ := c.Get("email")
+	rol, _ := c.Get("rol")
+	c.JSON(http.StatusOK, gin.H{"username": username, "email": email, "rol": rol})
+}
+
+func userInteractions(c *gin.Context) {
+	loged, _ := c.Get("loged")
+	c.JSON(http.StatusOK, gin.H{"last Loged": loged})
 }
