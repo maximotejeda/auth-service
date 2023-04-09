892fd36fa4f11317f177636a8c92b4975628c900
maximo tejeda <maximotejeda@gmail.com>
2023-04-09 13:17:36 -0400

    EDIT server start from env variables

--- main/main.go
+++ main/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"os"
+
 	r "github.com/maximotejeda/auth-service/external/v0.1/router"
 )
 
+var (
+	port = os.Getenv("SERVERPORT")
+	addr = os.Getenv("SERVERADDr")
+)
+
 func main() {
+
 	router := r.NewRouter()
 
 	r.AuthAddRoutes(router)
 	r.UserAddRoutes(router)
 	r.AdminAddRoutes(router)
 
-	r.R.Run(":8083")
+	r.R.Run(addr + ":" + port)
 }
